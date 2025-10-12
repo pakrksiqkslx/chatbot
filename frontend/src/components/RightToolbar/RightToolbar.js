6186d26a6b5712c0431dcadd1b7193a01a848ecd
sungbin <jddhsk18@naver.com>
2025-10-12 20:09:59 +0900

    로그인 아이콘/로그아웃버튼추가

--- frontend/src/components/RightToolbar/RightToolbar.js
+++ frontend/src/components/RightToolbar/RightToolbar.js
@@ -9,8 +9,9 @@ import React from 'react';
 import './RightToolbar.css';
 
 
-export default function RightToolbar({ onToggle, sidebarOpen, onNewChat, onLoginClick }) {
+export default function RightToolbar({ onToggle, sidebarOpen, onNewChat, onLoginClick, onLogoutClick, isLoggedIn }) {
   const toolbarClass = sidebarOpen ? 'rightbar-toolbar-22 rightbar-toolbar-22--left' : 'rightbar-toolbar-22';
+  
   return (
     <nav className={toolbarClass} aria-label="오른쪽 툴바">
       <div className="rightbar-toolbar-22__top">
@@ -22,10 +23,47 @@ export default function RightToolbar({ onToggle, sidebarOpen, onNewChat, onLogin
         </button>
       </div>
       <div className="rightbar-toolbar-22__bottom">
-        {/* 로그인 버튼: 시계 버튼 위에 위치 */}
-        <button className="rightbar-toolbar-22__btn" onClick={onLoginClick} aria-label="로그인">
-          <img src="/아이콘/메인아이콘.png" alt="로그인" className="rightbar-toolbar-22__icon" />
-        </button>
+        {/* 로그인되지 않았을 때: 로그인 버튼 */}
+        {!isLoggedIn && (
+          <button 
+            className="rightbar-toolbar-22__btn" 
+            onClick={onLoginClick} 
+            aria-label="로그인"
+          >
+            <img 
+              src="/아이콘/로그인.png" 
+              alt="로그인" 
+              className="rightbar-toolbar-22__icon rightbar-toolbar-22__auth-icon" 
+            />
+          </button>
+        )}
+        
+        {/* 로그인되었을 때: 사용자 프로필 아이콘 + 로그아웃 버튼 */}
+        {isLoggedIn && (
+          <>
+            <button 
+              className="rightbar-toolbar-22__btn" 
+              aria-label="사용자 프로필"
+            >
+              <img 
+                src="/아이콘/로그인했을때.png" 
+                alt="사용자 프로필" 
+                className="rightbar-toolbar-22__icon rightbar-toolbar-22__auth-icon" 
+              />
+            </button>
+            <button 
+              className="rightbar-toolbar-22__btn" 
+              onClick={onLogoutClick} 
+              aria-label="로그아웃"
+            >
+              <img 
+                src="/아이콘/로그아웃.png" 
+                alt="로그아웃" 
+                className="rightbar-toolbar-22__icon rightbar-toolbar-22__auth-icon rightbar-toolbar-22__logout-icon" 
+              />
+            </button>
+          </>
+        )}
         <button className="rightbar-toolbar-22__btn" aria-label="활동">
           <img src="/아이콘/시계.png" alt="활동" className="rightbar-toolbar-22__icon" />
         </button>
